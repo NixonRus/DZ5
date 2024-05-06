437710c41d8ea4063f70a2d4bd8a4b22e06cd135
NixonRus <167446421+NixonRus@users.noreply.github.com>
2024-05-06 23:22:48 +0700

    Add files via upload

---
+++ HomeWork5.py
@@ -0,0 +1,14 @@
+my_list = ['Toyota', 'Honda', 'Nissan', 'Mazda', "Subaru", 'Mitsubishi', 'Suzuki']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'kawasaki'
+print(my_list)
+
+my_dict = {'Toyota': 'Supra', 'Honda': 'Integra',
+           'Mazda': 'RX-8', 'Subaru': 'Legacy', 'Mitsubishi': 'Evolution', 'Suzuki': 'Bandit' }
+print(my_dict)
+print(my_dict['Mazda'])
+my_dict['Mazda'] = 'CX-5'
+print(my_dict)
